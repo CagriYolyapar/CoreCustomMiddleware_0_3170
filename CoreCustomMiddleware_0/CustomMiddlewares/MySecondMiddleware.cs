f26c6d277efbe36f01c529625433e731f2fbddc5
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-03-11 14:44:52 +0300

    Add project files.

---
+++ CoreCustomMiddleware_0/CustomMiddlewares/MySecondMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace CoreCustomMiddleware_0.CustomMiddlewares
+{
+    public class MySecondMiddleware
+    {
+        RequestDelegate _requestDelegate;
+
+        public MySecondMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _requestDelegate.Invoke(context); 
+
+            if (context.Response.StatusCode == StatusCodes.Status200OK)
+                await context.Response.WriteAsync("Basarılı");
+            else
+                await context.Response.WriteAsync("Bulunamadı");
+        }
+    }
+}
